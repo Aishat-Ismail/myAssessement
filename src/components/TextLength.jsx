7b96039471532e76529aa68e086a499b4008d893
Dev_Aishah <152623999+Aishat-Ismail@users.noreply.github.com>
2024-03-21 18:23:23 +0100

    Add files via upload

---
+++ src/components/TextLength.jsx
@@ -0,0 +1,12 @@
+
+
+export default function TextLength (props){
+    return (
+        <>
+            <h1>Input Value: <br /> <span>{props.textOutput}</span> </h1>
+            <input  onChange={props.changeValue} type="text" />
+        </>
+    )
+}
+
+
---
+++ src/components/TheTextLength.jsx
@@ -0,0 +1,9 @@
+
+export default function TheTextLength (props){
+    return(
+        <>
+        <p>{props.children} {props.theLength}</p>
+        <h3>{props.long}</h3>
+        </>
+    )
+}
